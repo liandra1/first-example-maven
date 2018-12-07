eb6541e6f7e5a1814b8e585ec5f166536d1dfae6
Linda Grönberg <linda.groenberg@gmail.com>
2018-12-07 13:42:28 +0100

    First commit

---
+++ src/main/java/com/kvalit18/example/first_example_with_maven/Calculator.java
@@ -0,0 +1,9 @@
+package com.kvalit18.example.first_example_with_maven;
+
+public class Calculator {
+	public double addition(double firstNumber, double secondNumber) {
+		double returnValue = 0;
+		returnValue = firstNumber + secondNumber;
+		return returnValue;
+	}
+}
---
+++ src/test/java/com/kvalit18/example/first_example_with_maven/CalculatorTest.java
@@ -0,0 +1,34 @@
+package com.kvalit18.example.first_example_with_maven;
+
+import static org.junit.Assert.*;
+
+import java.util.Random;
+
+import org.junit.Test;
+
+public class CalculatorTest {
+	Calculator myCalculator = new Calculator();
+
+	
+	@Test
+	public void testAddition10times() {
+		double oracle = 0;
+		double random1 = 0.0;
+		double random2 = 0.0;
+		Random r = new Random();
+		
+		for (int i = 0; i < 10; i++) {
+			random1 = r.nextDouble()*10;
+			random2 = r.nextDouble()*10;
+			oracle = random1 + random2;
+			assertEquals(myCalculator.addition(random1, random2),oracle,0);	
+			System.out.println("Testing addition method with " + random1 + " and "+ random2+" = "+oracle);
+		}
+		
+		
+		
+	}
+
+
+
+}
